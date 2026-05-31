e47475a266387834a12e21d303f6dee14011f992
Eliasraad <eliasraad957@gmail.com>
2026-05-31 13:47:51 +0300

    second commit

--- run.py
+++ run.py
@@ -5,7 +5,6 @@ if __name__ == "__main__":
     uvicorn.run("app.main:app", host="127.0.0.1", port=8000, reload=True)
 
 
-git commit -m "first commit"
+
 git branch -M main
-git remote add origin https://github.com/Eliasraad/haris_backend.git
-git push -u origin main
+
